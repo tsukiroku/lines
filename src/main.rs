ad0867ea95cec283a25b6e188ca162480de660d2
tsukiroku <tsukiroku@yahoo.com>
2022-06-04 14:50:28 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod args_parse;
+mod core;
+
+use crate::{
+    args_parse::Args,
+    core::{lines, LinesOption},
+};
+use clap::Parser;
+
 fn main() {
-    
+    let args = Args::parse();
+    println!(
+        "{}",
+        lines(LinesOption {
+            directory: args.directory,
+            ignore: args.ignore,
+        })
+    );
 }
