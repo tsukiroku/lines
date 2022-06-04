1024e6d33052796b5aac54feda9eeeaa7277331d
tsukiroku <tsukiroku@yahoo.com>
2022-06-04 14:50:21 +0900

    Create args_parse.rs

---
+++ src/args_parse.rs
@@ -0,0 +1,11 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author)]
+pub struct Args {
+    #[clap(short, long, default_value = "./")]
+    pub directory: String,
+
+    #[clap(short, long)]
+    pub ignore: Option<String>,
+}
